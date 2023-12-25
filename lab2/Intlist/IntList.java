854c7ba36de05a5d0d5b1551e6d203538785da38
xuhaoran <NeoLeeAI@outlook.com>
2023-12-25 21:21:06 +0800

    done lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -97,6 +97,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A.equals(null)) {
+            return B;
+        }
         IntList res = new IntList(A.first, null);
         IntList pre = res;
         A = A.rest;
