26385a183b5cd2aa1805087eda676ea8774b891d
xuhaoran <NeoLeeAI@outlook.com>
2024-03-02 17:29:04 +0800

    2024.3.2

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -9,30 +9,38 @@ public class TestArrayDequeGold {
         String msg = "";
 
         for (int i = 0; i < 100; i += 1) {
-            int numberBetweenZeroAndFour = StdRandom.uniform(0, 4);
+            int flag = StdRandom.uniform(0, 4);
+            Integer val = Integer.valueOf(StdRandom.uniform(100));
 
-            if (numberBetweenZeroAndFour == 0) {
-                sad.addLast(i);
-                ad.addLast(i);
+            if (flag == 0) {
+                sad.addLast(val);
+                ad.addLast(val);
                 msg = msg + "addLast(" + i + ")" + "\n";
-            } else if (numberBetweenZeroAndFour == 1) {
-                sad.addFirst(i);
-                ad.addFirst(i);
-                msg = msg + "addFirst(" + i + ")" + "\n";
-            } else if (numberBetweenZeroAndFour == 2) {
-                assertEquals(msg, ad.isEmpty(), sad.isEmpty());
-                if (ad.isEmpty()) break;
-                Integer sadLast = sad.removeLast();
-                Integer adLast = ad.removeLast();
-                msg = msg + "removeLast(" + ")" + "\n";
-                assertEquals(msg, sadLast, adLast);
             } else {
-                assertEquals(msg, ad.isEmpty(), sad.isEmpty());
-                if (ad.isEmpty()) break;
-                Integer sadFirst = sad.removeFirst();
-                Integer adFirst = ad.removeFirst();
-                msg = msg + "removeFirst(" + ")" + "\n";
-                assertEquals(msg, sadFirst, adFirst);
+                if (flag == 1) {
+                    sad.addFirst(val);
+                    ad.addFirst(val);
+                    msg = msg + "addFirst(" + i + ")" + "\n";
+                } else {
+                    if (flag == 2) {
+                        if (!ad.isEmpty()) {
+                            assertEquals(msg, ad.isEmpty(), sad.isEmpty());
+                            if (ad.isEmpty()) break;
+                            Integer ac = sad.removeLast();
+                            Integer exp = ad.removeLast();
+                            msg = msg + "removeLast(" + ")" + "\n";
+                            assertEquals(msg, exp, ac);
+                        }
+                    } else {
+                        if (!ad.isEmpty()) {                        assertEquals(msg, ad.isEmpty(), sad.isEmpty());
+                            if (ad.isEmpty()) break;
+                            Integer ac = sad.removeFirst();
+                            Integer exp = ad.removeFirst();
+                            msg = msg + "removeFirst(" + ")" + "\n";
+                            assertEquals(msg, exp, ac);
+                        }
+                    }
+                }
             }
         }
     }
