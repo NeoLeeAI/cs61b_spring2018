7fbf78a32c92148b338ee0ff123069320323c1d6
xuhaoran <NeoLeeAI@outlook.com>
2024-03-06 15:08:12 +0800

    done 24.3.6

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -60,7 +60,7 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * Return oldest item, but don't remove it.
      */
     public T peek() {
-        if (isFull()) {
+        if (isEmpty()) {
             throw new RuntimeException("Ring Buffer Overflow");
         }
         return rb[first];
